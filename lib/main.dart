1a40754214c3b6a9c93950b57a009c88034ee62a
FlutLab.io <>
2021-12-30 08:29:57 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,59 +12,48 @@ class SecondClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Second Class',
-            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          shape: StadiumBorder(),
-          shadowColor: Colors.red,
-          elevation: 25,
+      appBar: AppBar(
+        title: Text(
+          'Second Class',
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
         ),
+        centerTitle: true,
+        shape: StadiumBorder(),
+        shadowColor: Colors.red,
+        elevation: 25,
+      ),
+
 //appp bar closing
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(20),
-                height: 200,
-                width: 400,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(32), color: Colors.teal),
-                child: Center(
-                    child: Column(
-                  children: [
-                    Chip(
-                      elevation: 20,
-                      padding: EdgeInsets.all(8),
-                      backgroundColor: Colors.greenAccent[100],
-                      shadowColor: Colors.black,
-                      avatar: CircleAvatar(
-                        backgroundImage: NetworkImage("https://pbs.twimg.com/profile_images/1304985167476523008/QNHrwL2q_400x400.jpg"), //NetwordImage
-                      ), //CircleAvatar
-                      label: Text(
-                        'GeeksforGeeks',
-                        style: TextStyle(fontSize: 20),
-                      ), //Text
-                    ),
-                  ],
-                )),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.all(20),
-                height: 200,
-                width: 400,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(32), color: Colors.teal),
-                child: Center(
-                  child: Text('TSM Cargo'),
-                ),
-              ),
-            ],
-          ),
-        ));
+      body: Column(
+        children: [
+          chip('Health', Color(0xFFff8a65)),
+          chip('Food', Color(0xFF4fc3f7)),
+          chip('Lifestyle', Color(0xFF9575cd)),
+          chip('Sports', Color(0xFF4db6ac)),
+          chip('Nature', Color(0xFF5cda65)),
+          chip('Learn', Color(0xFFacbb65)),
+        ],
+      ),
+    );
   }
+}
+
+Widget chip(String label, Color color) {
+  return Chip(
+    labelPadding: EdgeInsets.all(5.0),
+    avatar: CircleAvatar(
+      backgroundColor: Colors.grey.shade600,
+      child: Text(label[0].toUpperCase()),
+    ),
+    label: Text(
+      label,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+    ),
+    backgroundColor: color,
+    elevation: 6.0,
+    shadowColor: Colors.grey[60],
+    padding: EdgeInsets.all(6.0),
+  );
 }
