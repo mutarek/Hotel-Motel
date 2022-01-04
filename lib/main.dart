50a2130e2c426efd5e5fb25733c1f1d2fd37c765
FlutLab.io <>
2022-01-04 07:31:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'travelCard.dart';
 
 void main() {
@@ -49,118 +48,120 @@ class _TravelAppState extends State<TravelApp> {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Let's start by adding the text
-            Text(
-              "Hootel & Motel",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26.0,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            Text(
-              "Pick your destination",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            //Now let's add some elevation to our text field
-            //to do this we need to wrap it in a Material widget
-            Material(
-              elevation: 10.0,
-              borderRadius: BorderRadius.circular(30.0),
-              shadowColor: Color(0x55434343),
-              child: TextField(
-                textAlign: TextAlign.start,
-                textAlignVertical: TextAlignVertical.center,
-                decoration: InputDecoration(
-                  hintText: "Search for Hotel, Flight...",
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.black54,
-                  ),
-                  border: InputBorder.none,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Let's start by adding the text
+              Text(
+                "Hootel & Motel",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-            ),
-            SizedBox(height: 30.0),
-            //Now let's Add a Tabulation bar
-            DefaultTabController(
-              length: 3,
-              child: Expanded(
-                child: Column(
-                  children: [
-                    TabBar(
-                      indicatorColor: Color(0xFFFE8C68),
-                      unselectedLabelColor: Color(0xFF555555),
-                      labelColor: Color(0xFFFE8C68),
-                      labelPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                      tabs: [
-                        Tab(
-                          text: "Hotels",
-                        ),
-                        Tab(
-                          text: "Best",
-                        ),
-                        Tab(
-                          text: "Offer",
-                        ),
-                      ],
+              Text(
+                "Pick your destination",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              //Now let's add some elevation to our text field
+              //to do this we need to wrap it in a Material widget
+              Material(
+                elevation: 10.0,
+                borderRadius: BorderRadius.circular(30.0),
+                shadowColor: Color(0x55434343),
+                child: TextField(
+                  textAlign: TextAlign.start,
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    hintText: "Search for Hotel, Flight...",
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.black54,
                     ),
-                    SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                      height: 300.0,
-                      child: TabBarView(
-                        children: [
-                          //Now let's create our first tab page
-                          Container(
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              children: [
-                                //Now let's add and test our first card
-                                travelCard(urls[0], "Luxary Hotel", "Caroline", 3),
-                                travelCard(urls[5], "Plaza Hotel", "Italy", 4),
-                                travelCard(urls[2], "Safari Hotel", "Africa", 5),
-                              ],
-                            ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0),
+              //Now let's Add a Tabulation bar
+              DefaultTabController(
+                length: 3,
+                child: Expanded(
+                  child: Column(
+                    children: [
+                      TabBar(
+                        indicatorColor: Color(0xFFFE8C68),
+                        unselectedLabelColor: Color(0xFF555555),
+                        labelColor: Color(0xFFFE8C68),
+                        labelPadding: EdgeInsets.symmetric(horizontal: 8.0),
+                        tabs: [
+                          Tab(
+                            text: "Hotels",
                           ),
-                          Container(
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              children: [
-                                //Here you can add what ever you want
-                                travelCard(urls[6], "Visit Rome", "Italy", 4),
-                                travelCard(urls[8], "Visit Sidi bou Said", "Tunsia", 4),
-                              ],
-                            ),
+                          Tab(
+                            text: "Best",
                           ),
-                          Container(
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              children: [],
-                            ),
+                          Tab(
+                            text: "Offer",
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Container(
+                        height: 300.0,
+                        child: TabBarView(
+                          children: [
+                            //Now let's create our first tab page
+                            Container(
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: [
+                                  //Now let's add and test our first card
+                                  travelCard(urls[0], "Luxary Hotel", "Caroline", 3),
+                                  travelCard(urls[5], "Plaza Hotel", "Italy", 4),
+                                  travelCard(urls[2], "Safari Hotel", "Africa", 5),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: [
+                                  //Here you can add what ever you want
+                                  travelCard(urls[6], "Visit Rome", "Italy", 4),
+                                  travelCard(urls[8], "Visit Sidi bou Said", "Tunsia", 4),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: [],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
